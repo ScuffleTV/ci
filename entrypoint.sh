7f1d193502ca994afe3cf98322d1afabdcc4f084
Troy Benson <troybensonsa@gmail.com>
2023-12-29 04:03:47 +0000

    fix: args env

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,14 @@ cmd=$1
 # Remove the first argument from the list.
 shift
 
-# Check if there are additional script arguments.
-if [ $# -eq 0 ]; then
-    args=()
-else
-    args=("$@")
-fi
-
 # Also accept args from the environment.
-if [ -n "$INPUT_ARGS" ]; then
+if [ -n "$ARGS" ]; then
     # Use eval to execute the command with INPUT_ARGS.
     # The exec replaces the current shell with the command.
     # Quoting is important to handle spaces and special characters.
-    eval "exec $cmd $INPUT_ARGS ${args[*]}"
+    eval "exec $cmd $ARGS"
 else
     # If INPUT_ARGS is not set, use exec to replace the shell
     # with the command and the direct script arguments.
-    exec "$cmd" "${args[@]}"
+    exec "$cmd" "$@"
 fi
